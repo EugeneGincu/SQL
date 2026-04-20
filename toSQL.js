f70477e7da45e50725220785f04006579c4cfb4f
Eugene <eugene.gincu@gmail.com>
2026-04-19 23:56:55 -0600

    integrated all database and table creation into 1 file

--- toSQL.js
+++ toSQL.js
@@ -12,12 +12,16 @@ async function importData() {
     await conn.query('CREATE DATABASE IF NOT EXISTS nutrition');
 	await conn.query('USE nutrition');
 
-    const data = JSON.parse(fs.readFileSync('Data/Nutrition.json', 'utf8'));
+    const data = JSON.parse(fs.readFileSync('./Data/Nutrition.json', 'utf8'));
     
-    data.forEach(row => row.Channels = row.Channels.replaceAll('?','').replaceAll('Pancreas','PN').replaceAll('Adrenal','AD').replaceAll('Prostate','PS'));
+    data.forEach(row => {
+			row.Channels = row.Channels.replaceAll('?','').replaceAll('Pancreas','PN').replaceAll('Adrenal','AD').replaceAll('Uterus','PS').replaceAll(', others','');
+			row.Name = row.Name.split('-')[0].split('(')[0].trim();
+		}
+	);
 
     await conn.execute('CREATE TABLE IF NOT EXISTS nutrition (' +
-        'id INT AUTO_INCREMENT PRIMARY KEY,' +
+        'id INT PRIMARY KEY,' +
         'name VARCHAR(500),' +
         'temp VARCHAR(100),' +
         'type VARCHAR(100),' +
@@ -29,20 +33,98 @@ async function importData() {
         'chakra VARCHAR(200),' +
         'notes TEXT' +
         ')  ');
-
-    const rows = data.map(item => [
-        item.Name, item.Temp, item.Type, item.Channels, item.Tonifies,
+		
+	let index = 0;
+	
+    const nutritionRows = data.map(item => [
+        index++, item.Name, item.Temp, item.Type, item.Channels, item.Tonifies,
         item.Properties, item.Pacifies, item.Aggravates, item.Chakra, item.Notes
     ]);
 
     await conn.query(
-        'INSERT INTO nutrition (name, temp, type, channels, tonifies, properties, pacifies,'+
+        'INSERT INTO nutrition (id, name, temp, type, channels, tonifies, properties, pacifies,'+
         'aggravates, chakra, notes) VALUES ?',
-        [rows]
+        [nutritionRows]
     );
 
-    console.log(`Inserted ${rows.length} rows.`);
+    console.log(`Inserted ${nutritionRows.length} rows.`);
+	
+	//Tables
+	const propertyRows = [];
+	const channelRows = [];
+	
+	
+	nutritionRows.forEach(row =>{ 
+    if (row[6] && row[6].trim()) {
+        row[6].split(',').forEach(prop => {
+            propertyRows.push([row[0], prop.trim()]);
+        });
+    }
+	
+	if (row[4] && row[4].trim()) {
+		row[4].split(',').forEach(prop => {
+			channelRows.push([row[0], prop.trim()]);
+		});
+	}
+	
+	});
+	
+	await createPropertiesTable(conn, propertyRows);
+	await createChannelsTable(conn, channelRows);
+
+
+//fs.writeFileSync('Data/Properties.json', JSON.stringify(propertyRows, null, 2));
+//console.log(`Generated ${propertyRows.length} property rows.`);
+	
     await conn.end();
 }
+
+async function createPropertiesTable(conn, propertyRows) {
+	//Property table creation and insertion
+	await conn.execute('CREATE TABLE properties (' +
+		'ind INT AUTO_INCREMENT,' +
+		'id INT,' +
+		'property VARCHAR(100),' +
+		'CONSTRAINT pk_properties_ind PRIMARY KEY (ind),' +
+		'CONSTRAINT fk_properties_id FOREIGN KEY (id)' +
+		'REFERENCES nutrition (id)' +
+		')'
+	);
+	
+	await conn.query(
+		'INSERT INTO properties' +
+		'(id, property)' +
+		'VALUES ?',
+		[propertyRows]
+	);
+	
+	console.log(`Inserted ${propertyRows.length} rows`);
+
+	
+		
+}
+
+async function createChannelsTable(conn, channelRows) {
+	//Channel table creation and insertion
+	await conn.execute('CREATE TABLE channels (' +
+		'ind INT AUTO_INCREMENT,' +
+		'id INT,' +
+		'channel ENUM("LU", "ST", "SP", "HT", "SI", "LI", "UB", "KD", "LV", "GB", "PC", "SJ", "PN", "AD", "PS"),' +
+		'CONSTRAINT pk_channels_ind PRIMARY KEY (ind),' +
+		'CONSTRAINT fk_channels_id FOREIGN KEY (id)' +
+		'REFERENCES nutrition (id)' +
+		')'
+	);
+	
+	await conn.query(
+		'INSERT INTO channels' +
+		'(id, channel)' +
+		'VALUES ?',
+		[channelRows]
+	);
+	
+	console.log(`Inserted ${channelRows.length} rows`);
+}
+
 
 importData();
