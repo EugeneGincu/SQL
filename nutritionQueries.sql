beb88391673df3f73d4b913986b0c54cae5d139b
Eugene <eugene.gincu@gmail.com>
2026-04-26 20:59:25 -0600

    added group concat query

--- nutritionQueries.sql
+++ nutritionQueries.sql
@@ -1,3 +1,9 @@
 use nutrition;
-select * from nutrition limit 5;
-select id from nutrition limit 5;
+select n.id, n.name, group_concat(distinct c.channel order by c.channel asc), group_concat(distinct p.property)
+from nutrition as n
+inner join channels as c
+on n.id = c.id
+inner join properties as p
+on n.id = p.id
+where n.id < 10
+group by n.id, n.name;
